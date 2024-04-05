613dd1edc5183d62f32f8a71029d60365b888fc1
Florian Dupuis <floriandupuis1996@gmail.com>
2024-04-05 15:09:58 +0200

    feat: fix import page

--- app/projects/(single)/absolunet-vuse/page.tsx
+++ app/projects/(single)/absolunet-vuse/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 import ENVersion from "./en-version.mdx"
-import FRVersion from "./fr-version.mdx"
 
-export default function ContentPage({ params: { lang } }: { params: { lang: string } }) {
-	if (lang === "en") return <ENVersion />
-	if (lang === "fr") return <FRVersion />
+export default function ContentPage() {
+	return <ENVersion />
 }
